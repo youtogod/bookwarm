8bccf05457132ef887a26eb0f7e79383d4e94744
yun-tao.yang <yun-tao.yang@hpe.com>
2018-06-12 17:31:41 +0800

    config

---
+++ src/main/java/com/yuntao/bookworm/ReaderRepository.java
@@ -0,0 +1,9 @@
+package com.yuntao.bookworm;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by yangyun on 6/12/2018.
+ */
+public interface ReaderRepository extends JpaRepository<Reader, String> {
+}
